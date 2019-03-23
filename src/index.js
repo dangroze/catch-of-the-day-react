ed65ced20a686485d40b875a824286bd7eb68e35
Dan Groze <dangroze@gmail.com>
2019-03-23 22:05:21 +0000

    :sparkles:Add StorePicker component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// let's go!
-alert('Hey!')
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'
+
+render(< StorePicker />, document.getElementById('main'));
